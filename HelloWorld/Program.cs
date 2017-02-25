2ad7b14d1216da8d5f7b20751102c4e63b906118
Evans Okosodo <EOkosodo@fleettechltd.com>
2017-02-25 16:12:37 +0100

    create c# hello world project

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print hello world 
+            Console.WriteLine("What is your name");
+            string input = Console.ReadLine();
+            Console.WriteLine("You typed: " + input);
+
+            Console.ReadLine();
+        }
+    }
+}
